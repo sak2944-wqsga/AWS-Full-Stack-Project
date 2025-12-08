6a5a004543eff48bf29207df2708b57c26cdef88
Shubham <sak2944@gmail.com>
2025-12-08 10:36:32 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://10.128.0.2";
+const API_BASE_URL = "http://34.172.216.134";
 // const API_BASE_URL = "http://aa7d431074bc74f7bb7100a9503548ad-1352086311.us-east-1.elb.amazonaws.com";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
