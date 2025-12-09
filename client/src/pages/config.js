23238c89761dc43d7d6885be7b639c9735cd3bc6
Shubham <sak2944@gmail.com>
2025-12-09 09:29:25 +0530

    Update config.js
    Azure

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://136.116.15.55";
+const API_BASE_URL = "http://4.186.27.43";
 // const API_BASE_URL = "http://aa7d431074bc74f7bb7100a9503548ad-1352086311.us-east-1.elb.amazonaws.com";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
